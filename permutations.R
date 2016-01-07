9da22f6ab2cee3f83d5f14be3261b97a6a584034
Alan <amwill04@users.noreply.github.com>
2016-01-07 12:12:11 +0000

    Created permutations.R

---
+++ permutations.R
@@ -0,0 +1,12 @@
+perms <- function(x, loc){
+  # Function to create a list of all possible permitations.
+  # Saves csv file.
+  temp <- list()
+  opts <- length(x)
+ for (i in 1:opts) {
+   temp[[i]] <- data.frame(Combinations = apply(combn(x,i), 2,
+                                           paste, collapse = ", "))
+ }
+  temp <- do.call("rbind", temp)
+  write.csv(temp, loc,row.names = FALSE, quote = FALSE)
+}
